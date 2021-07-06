9e2f2bdf6fadbfef21e7eb465c3cf1a2dc1c1ea2
Rohit kumar mishra <68419435+Rohit98112@users.noreply.github.com>
2021-07-06 22:35:36 +0530

    Day-7

--- Day7/special_no.cpp
+++ Day7/special_no.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 int main()
 {
-     int n=3,m=3;
-     vector<vector<int> >v= {{3,7,8},
-                              {9,11,13},
-                              {15,16,17}};
+    
+     vector<vector<int> >v= {{1,10,4,2},
+                              {9,3,8,7},
+     
+                             {15,16,17,12}};
+      int n=3,m=4;
     int min,max;
     int k=0,res;
     for(int i=0;i<n;i++)
     {
         min=INT_MIN;max=INT_MAX;k=0;
-        for(int j=0;j<n;j++)
+        for(int j=0;j<m;j++)
         {
             if(v[i][j]<max){
                 max=v[i][j];
